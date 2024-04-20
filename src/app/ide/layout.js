9b2b0f982d8c9113bcd47d9701be95a886c62683
xin <anxanxx@gmail.com>
2024-04-21 01:44:29 +0800

    Refactor layout and page components for responsive design in layout.js and page.js files

--- src/app/ide/layout.js
+++ src/app/ide/layout.js
@@ -10,7 +10,7 @@ export default function ProjectLayout({ children }) {
     <div className="w-10/12 m-auto mt-20 relative flex-col flex gap-6">
       <nav
         className={
-          "w-10/12 m-auto flex justify-between fixed text-2xl md:text-4xl z-10"
+          "w-10/12 m-auto flex justify-between fixed text-2xl md:text-3xl z-10"
         }
       >
         <Link href={"/"} className=" flex ">
@@ -21,14 +21,14 @@ export default function ProjectLayout({ children }) {
         </h1>
       </nav>
 
-      <div className={"grid grid-cols-10 relative mt-14 mb-14 "}>
-        <div className="md:fixed grid grid-cols-10 w-10/12 mb-4">
-          <nav className=" md:flex flex-col gap-2 md:gap-4 lg:gap-6 text-xl md:text-2xl lg:text-4xl col-span-2">
+      <div className={"grid grid-cols-10 gap-4 relative mt-14 mb-14 "}>
+        <div className="md:w-10/12 md:fixed md:col-span-2 col-span-full mb-4 ">
+          <nav className=" flex flex-col gap-2 md:gap-4 md:col-span-2 lg:gap-6 text-xl md:text-2xl lg:text-3xl w-full md:w-2/12 ">
             {IDEProjectData.map((project) => (
               <Link
                 href={`/ide/${project.id}`}
                 key={project.id}
-                className={"flex justify-items-center relative"}
+                className={"flex justify-items-center"}
               >
                 {currentPageId === project.id ? (
                   <i className="ri-arrow-right-line  absolute md:-left-10 -left-5"></i>
@@ -36,7 +36,7 @@ export default function ProjectLayout({ children }) {
                   ""
                 )}
                 <p
-                  className={`t ${
+                  className={` ${
                     currentPageId === project.id ? "underline" : ""
                   }`}
                   key={project.id}
--- src/app/ui/layout.js
+++ src/app/ui/layout.js
@@ -10,7 +10,7 @@ export default function ProjectLayout({ children }) {
     <div className="w-10/12 m-auto mt-20 relative flex-col flex gap-6">
       <nav
         className={
-          "w-10/12 m-auto flex justify-between fixed text-2xl md:text-4xl z-10"
+          "w-10/12 m-auto flex justify-between fixed text-2xl md:text-3xl z-10"
         }
       >
         <Link href={"/"} className=" flex ">
@@ -21,9 +21,9 @@ export default function ProjectLayout({ children }) {
         </h1>
       </nav>
 
-      <div className={"grid grid-cols-10 relative mt-14 mb-14 "}>
-        <div className="md:fixed grid grid-cols-10 w-10/12 mb-4">
-          <nav className=" md:flex flex-col gap-2 md:gap-4 lg:gap-6 text-xl md:text-2xl lg:text-4xl col-span-2">
+      <div className={"grid grid-cols-10 gap-4 relative mt-14 mb-14 "}>
+        <div className="md:w-10/12 md:fixed md:col-span-2 col-span-full mb-4">
+          <nav className="flex flex-col gap-2 md:gap-4 md:col-span-2 lg:gap-6 text-xl md:text-2xl lg:text-3xl w-full md:w-2/12 ">
             {UIProjectData.map((project) => (
               <Link
                 href={`/ui/${project.id}`}
@@ -36,7 +36,7 @@ export default function ProjectLayout({ children }) {
                   ""
                 )}
                 <p
-                  className={`t ${
+                  className={`${
                     currentPageId === project.id ? "underline" : ""
                   }`}
                   key={project.id}
