4b190b9060214e4ecd9791589a1d8636eee6372c
XIN <60823033+anxndsgn@users.noreply.github.com>
2024-07-03 16:37:11 +0800

    add google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,14 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Background from "@/components/background";
-import "remixicon/fonts/remixicon.css";
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Background from '@/components/background';
+import 'remixicon/fonts/remixicon.css';
+import Script from 'next/script';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "ANXN",
-  description: "designed by anxn",
+  title: 'ANXN',
+  description: 'designed by anxn',
 };
 
 export default function RootLayout({ children }) {
@@ -17,6 +18,16 @@ export default function RootLayout({ children }) {
         {children}
         <Background />
       </body>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-PSQKZWTBF8"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-PSQKZWTBF8');`}
+      </Script>
     </html>
   );
 }
